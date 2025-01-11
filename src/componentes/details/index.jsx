d17ee59eb91eb80fef17eec4b0d49e0247d9593c
Jean Vinicius <zjeanbrx@gmail.com>
2025-01-11 08:12:00 -0300

    details txt animação

--- src/componentes/details/index.jsx
+++ src/componentes/details/index.jsx
@@ -7,6 +7,8 @@ import arrow from '../../../public/icones/arrow.png';
 export default function Details ({titulo, children, cor}) {
   
   const [details, setDetails] = useState(false);
+
+  const detailsStyle = styles.details;
   const detailsClosed = styles.details_txt_closed;
   const detailsOpen = styles.details_txt_open;
   const arrowClosed = styles.arrow_closed;
@@ -17,8 +19,8 @@ export default function Details ({titulo, children, cor}) {
   }
 
   return (
-    <div className={styles.details}>
-      <button onClick={detailsInvert} className={cor}>{titulo} <Image className={details ? arrowOpen : arrowClosed} src={arrow} width={20} height={20} alt='arrow'/> </button>
+    <div className={`${detailsStyle} ${cor}`}>
+      <button onClick={detailsInvert}>{titulo} <Image className={details ? arrowOpen : arrowClosed} src={arrow} width={20} height={20} alt='arrow'/> </button>
       <div className={details ? detailsOpen : detailsClosed}>{children}</div>
     </div>
   );
